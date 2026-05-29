d71052efbe0d62d9070ec23e2e0a80ff9d70023c
Jay Gandhi <Jaygandhi813@gmail.com>
2026-05-29 16:00:57 +0530

    Create and cleanUP React folders

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>First React App</h1>
+      <p>in this i just clean up folders </p>
+    </div>
+  );
+}
+
+export default App;
